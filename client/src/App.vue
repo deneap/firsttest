2663f0c5249cec82f9ec75038208dc31c420e81e
deneap <denea.polevoi@gmail.com>
2020-06-04 09:29:39 +0300

    Tot ce am facut in clockify

---
+++ client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <v-app>
+    <page-header />
+    <main>
+      <!-- <v-container fluid>
+    <router-view><router-view/>
+    </v-containe> -->
+    </main>
+    </v-app>
+    </div>
+</template>
+
+<script>
+import PageHeader from '@/components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; 
+}
+.input-group--text-field input {
+  border-bottom: 1px solid black;
+}
+.toolbar_title {
+  color: white;
+}
+.btn_content {
+  color: white;
+}
+</style>
---
+++ server/src/controllers/AuthenticationController.js
@@ -0,0 +1,34 @@
+const {User} = require('../models')
+
+module.exports = {
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
+    }
+  },
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+
+      if (!user) {
+        return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
+    }
+  }
+}
